1e6798b9f37e6b5327453f9cf6a3a479d615152e
Md. Mostak Shahid <mostak.shahid@gmail.com>
2019-12-29 12:24:20 +0600

    Initial commit

---
+++ functions/widgets.php
@@ -0,0 +1,60 @@
+<?php
+//Add widgets area
+function portfolio_widgets_init(){
+	register_sidebar(array(
+		'id' => 'sidebar',
+		'name' => __('Sidebar for Post', 'portfolio'),
+		//'description' => __('Add widgets here to appear in your Left SideBar', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+		'after_widget' => '</div>'
+	));	
+	register_sidebar(array(
+		'id' => 'sidebar-page',
+		'name' => __('Sidebar for Page', 'portfolio'),
+		//'description' => __('Add widgets here to appear in your Left SideBar', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+		'after_widget' => '</div>'
+	));	
+	register_sidebar(array(
+		'id' => 'footer_1',
+		'name' => __('Footer Column 1', 'portfolio'),
+		'description' => __('Add widgets here to appear in your Footer Column 1', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h4 class="widget-title">',
+		'after_title' => '</h4>',
+		'after_widget' => '</div>'
+	));	
+	register_sidebar(array(
+		'id' => 'footer_2',
+		'name' => __('Footer Column 2', 'portfolio'),
+		'description' => __('Add widgets here to appear in your Footer Column 2', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h4 class="widget-title">',
+		'after_title' => '</h4>',
+		'after_widget' => '</div>'
+	));	
+	register_sidebar(array(
+		'id' => 'footer_3',
+		'name' => __('Footer Column 3', 'portfolio'),
+		'description' => __('Add widgets here to appear in your Footer Column 3', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h4 class="widget-title">',
+		'after_title' => '</h4>',
+		'after_widget' => '</div>'
+	));	
+	register_sidebar(array(
+		'id' => 'footer_4',
+		'name' => __('Footer Column 4', 'portfolio'),
+		'description' => __('Add widgets here to appear in your Footer Column 4', 'portfolio'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_title' => '<h4 class="widget-title">',
+		'after_title' => '</h4>',
+		'after_widget' => '</div>'
+	));		
+
+}
+add_action('widgets_init', 'portfolio_widgets_init');
